a51cc409554d3e0af02e21218972ff1914ee8be7
suliamon roheem <suleroheem88@gmail.com>
2024-07-08 15:51:56 +0100

    first commit

---
+++ src/time.jsx
@@ -0,0 +1,47 @@
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <div>Count: {state.count}</div>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </>
+  );
+}
+import React from 'react';
+import TaskItem from './TaskItem';
+
+const TaskList = ({ tasks, onDelete, onToggleComplete, onEdit }) => {
+  return (
+    <div className="task-list">
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggleComplete={onToggleComplete}
+          onEdit={onEdit}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
+
